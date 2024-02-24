6f00d899d170729aaaa703c8dc45dc6eedd8adab
rohit-mahajan1_psl <rohit_mahajan1@persistent.com>
2024-02-24 21:51:10 +0530

    minor fix

--- frontendNew/src/components/LeaderboardRankCard.jsx
+++ frontendNew/src/components/LeaderboardRankCard.jsx
@@ -15,18 +15,6 @@ function LeaderboardRankCard({
 }) {
   const isSocietySelected = selectedSociety.name === item.name;
   return (
-    // <div className="leaderboard-rank1">
-    //   <div className="leaderboard-feature-card">
-    //     <h2 className="">1. Society 1</h2>
-    //     <span>Address: xyz road, Pune- 411xx0</span>
-    //     <img
-    //       alt="image"
-    //       src="https://images.unsplash.com/photo-1512295767273-ac109ac3acfa?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDF8fHllbGxvdyUyMHRlY2h8ZW58MHx8fHwxNjI2MjU1NDk0&amp;ixlib=rb-1.2.1&amp;w=200"
-    //       className="leaderboard-image1"
-    //     />
-    //   </div>
-    // </div>
-
     <Card
       className={`m-auto bg-gradient-to-r ${
         isSocietySelected
