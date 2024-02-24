05c82daed4e0c3fd796a555dc518bc241e611d18
Aditya Wanere <aditya.wanere2000@gmail.com>
2024-02-24 23:32:22 +0530

    Updated Login and SignUp

--- frontendNew/src/components/Login.jsx
+++ frontendNew/src/components/Login.jsx
@@ -1,17 +1,11 @@
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-// import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
-import {
-  signInWithEmailAndPassword
-} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import { getUserDetails } from "../firebase/firestore";
 
@@ -19,10 +13,14 @@ export default function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
+    const email = data.get("email");
+    const password = data.get("password");
     try {
-      const signedInUser = await signInWithEmailAndPassword(auth, email, password);
+      const signedInUser = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const userDetails = await getUserDetails(signedInUser.user.uid);
       console.log("User is signed in", userDetails);
     } catch (err) {
@@ -35,49 +33,55 @@ export default function Login() {
   };
 
   return (
-    <div className="login-container">
-      <Container component="main" maxWidth="xs">
+    <div className="container-fluid bg-gradient-to-r from-slate-900 to-slate-700 p-[3vw]">
+      <Container component="main" maxWidth="xm">
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 2.5,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5" sx={{ color: "black" }}>
-            Login
-          </Typography>
+          <div className="text-center w-inline text-3xl font-bold text-white py-2 rounded mt-[20vh] mb-5">
+            <h3>Login</h3>
+          </div>
           <Box
             component="form"
             onSubmit={handleSubmit}
             noValidate
-            sx={{ mt: 1, margin: "5px" }}
+            justifyContent="center"
+            alignItems="center"
+            sx={{ mt: 1, alignItems: "center" }}
           >
             <TextField
+              variant="filled"
               margin="normal"
               required
               fullWidth
               id="email"
-              color="secondary"
               label="Email"
               name="email"
               autoComplete="email"
-              autoFocus
               style={{
-                backgroundColor: "#0074f0",
+                backgroundColor: "black",
+                border: "1px solid White",
+                borderRadius: "5px",
               }}
               InputProps={{
                 style: {
-                  color: "black",
+                  color: "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: "white",
                 },
               }}
             />
             <TextField
+              variant="filled"
               margin="normal"
               required
               fullWidth
@@ -85,14 +89,20 @@ export default function Login() {
               label="Password"
               type="password"
               id="password"
-              color="secondary"
               autoComplete="current-password"
               style={{
-                backgroundColor: "#0074f0",
+                backgroundColor: "black",
+                borderRadius: "5px",
+                border: "1px solid White",
               }}
               InputProps={{
                 style: {
-                  color: "black",
+                  color: "white",
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: "white",
                 },
               }}
             />
@@ -104,6 +114,7 @@ export default function Login() {
             >
               Sign In
             </Button>
+
             <Grid container>
               <Grid
                 item
